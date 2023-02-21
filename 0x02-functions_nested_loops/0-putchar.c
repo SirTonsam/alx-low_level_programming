c38f212c15f746b1256b08fcd4572cfb291c26cc
SirTonsam <Samutongai@gmail.com>
2023-02-21 15:22:03 -0800

    main

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  main - print _putchar
+ *  is wriiten by tonsam
+ *  Return: 0
+ */
+ int main(voi)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
