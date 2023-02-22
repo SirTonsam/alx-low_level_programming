4e857f1c7e7df4294cdefd5e5d63a47a394c7750
SirTonsam <Samutongai@gmail.com>
2023-02-22 13:25:13 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print alphabet_x10
+ * Return: void
+ */
+void print_alphabetx10(void)
+{
+	char c;
+	int i = 0;
+	
+	while(i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	
+	_putchar('\n');
+	i++;
+	}
+}}
