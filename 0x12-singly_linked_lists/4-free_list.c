6fde5170aad76ff9a8d69b1d3102c21a1c99bcfe
SirTonsam <Samutongai@gmail.com>
2023-03-30 15:39:44 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,20 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_list - fress linked lists
- * @head: head of the list
- * Return: no return
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
  */
 void free_list(list_t *head)
 {
-	list_t *aux;
-	list_t *aux2;
+ list_t *temp;
 
-	aux = head;
-	while (aux)
-	{
-	aux2 = aux;
-	free(aux->str);
-	free(aux);
-	aux = aux2->next;
-	}
+ while (head)
+ {
+ temp = head->next;
+ free(head->str);
+ free(head);
+ head = temp;
+ }
+}
 
