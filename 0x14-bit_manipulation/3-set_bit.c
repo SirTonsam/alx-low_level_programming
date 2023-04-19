40625c7f7ce49226071365802728047df7d00eab
SirTonsam <Samutongai@gmail.com>
2023-04-19 11:28:31 -0700

    3-set_bit.c correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,14 +4,13 @@
  * function that sets the value of a bit to 1 at a given index
  * Return: 1 if it worked, or -1 if an error occurred
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int a;
-
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	a = 1 << index;
-	*n = *n | a;
+
+	*n ^= (1 << index);
 
 	return (1);
 }
