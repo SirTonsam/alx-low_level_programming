a01d023cdde2a8cddc03b312c334a2c8cf011801
SirTonsam <samutongai@gmail.com>
2023-05-22 13:54:22 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,29 +1,27 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a doubly linked list.
- * @head: Pointer to a pointer to the head node of the list.
- * @n: The integer value to be stored in the new node.
+ * add_dnodeint - add a new node at the beginning of the linked list
+ * @head: double pointer to the beginning of the linked list
+ * @n: value to add to the new node
  *
- * Return: Address of the new element, or NULL if it failed.
+ * Return: pointer to the new node, or NULL on failure
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
-
-    if (*head != NULL)
-        (*head)->prev = new_node;
-
-    *head = new_node;
-
-    return (new_node);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	if (new->next != NULL)
+		(new->next)->prev = new;
+	return (new);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,37 +1,34 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a doubly linked list.
- * @head: Pointer to a pointer to the head node of the list.
- * @n: The integer value to be stored in the new node.
+ * add_dnodeint_end - adds a node to the end of a linked list
+ * @head: double pointer to the beginning of the linked list
+ * @n: value to add to new node
  *
- * Return: Address of the new element, or NULL if it failed.
+ * Return: pointer to the new node, or NULL on failure
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *temp;
+	dlistint_t *new, *tmp;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-    }
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = new_node;
-        new_node->prev = temp;
-    }
-
-    return (new_node);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
 }
